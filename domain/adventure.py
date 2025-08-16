e65546438786908a0572e18c3c12ad5f61b3e1aa
Nathan <n.pinheiro2004@gmail.com>
2025-08-16 21:21:55 +0200

    update adventures

--- domain/adventure.py
+++ domain/adventure.py
@@ -1,8 +1,16 @@
+from adventure import Adventure
+from map import Area, Map
+
+from dataclasses import dataclass
+
+@dataclass
 class Adventure:
-    """Represents an adventure which correspond to a full story."""
-    def __init__(self, id: int, title: str, description: str, min_players: int, max_players: int):
-        self.id = id
-        self.title = title
-        self.description = description
-        self.min_players = min_players
-        self.max_players = max_players
+    """
+    Class that store an adventure.
+    """
+    id: int
+    title: str
+    description: str
+    minPlayers: int
+    maxPlayers: int
+    map: Map
